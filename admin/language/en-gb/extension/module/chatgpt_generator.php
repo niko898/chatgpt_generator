e508bdea747ee7a863c6e4358d0cf4b075423d49
wildniko88@gmail.com <wildniko88@gmail.com>
2023-02-10 14:47:13 +0400

    chatgpt start

---
+++ admin/language/en-gb/extension/module/chatgpt_generator.php
@@ -0,0 +1,17 @@
+<?php
+// Heading
+$_['heading_title']    = 'ChatGPT plugin from Galocaffe';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified module!';
+$_['text_edit']        = 'ChatGPT plugin Settings';
+
+// Entry
+$_['entry_status']     = 'Status';
+$_['entry_api_key']     = 'ChatGPT Api key';
+$_['entry_prompt']     = 'Prompt';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify account module!';
+$_['error_api_key'] = 'The ChatGPT Api key field is required';
